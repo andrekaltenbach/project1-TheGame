baa610effa10e78242c4d86749c037798cdad105
Andre Kaltenbach <andre.kaltenbach@posteo.de>
2025-04-14 16:23:32 +0200

    add 'good items' functionality for MVP

--- js/main.js
+++ js/main.js
@@ -9,9 +9,7 @@ class Player {
   }
 
   updateUi() {
-    console.log('update UI');
     const playerElement = document.getElementById('player');
-    console.log(playerElement);
     playerElement.style.width = this.width + 'px';
     playerElement.style.height = this.height + 'px';
     playerElement.style.left = this.positionX + 'px';
@@ -34,8 +32,69 @@ class Player {
   }
 }
 
-const player = new Player();
+class Items {
+  constructor() {
+    this.width = 20;
+    this.height = 20;
+    this.positionX = Math.floor(Math.random() * 480 + 1);
+    this.positionY = Math.floor(Math.random() * 480 + 1);
+    this.itemElement = null;
+    this.createItem();
+    this.updateUi();
+  }
 
+  createItem() {
+    this.itemElement = document.createElement('div');
+    this.itemElement.className = 'item';
+    const pitch = document.getElementById('pitch');
+    pitch.appendChild(this.itemElement);
+  }
+
+  removeItem() {
+    this.itemElement.remove();
+  }
+
+  updateUi() {
+    this.itemElement.style.width = this.width + 'px';
+    this.itemElement.style.height = this.height + 'px';
+    this.itemElement.style.left = this.positionX + 'px';
+    this.itemElement.style.bottom = this.positionY + 'px';
+    this.itemElement.style.backgroundColor = '#33FF00';
+  }
+}
+
+const player = new Player();
+// generate items every 2s
+const itemsArr = [];
+setInterval(() => {
+  const item = new Items();
+  itemsArr.push(item);
+  if (itemsArr.length > 2) {
+    itemsArr[0].removeItem();
+    itemsArr.shift();
+  }
+}, 2000);
+
+// collision detection + remove item
+let gameScore = 0;
+setInterval(() => {
+  itemsArr.forEach((item) => {
+    if (
+      player.positionX < item.positionX + item.width &&
+      player.positionX + player.width > item.positionX &&
+      player.positionY < item.positionY + item.height &&
+      player.positionY + player.height > item.positionY
+    ) {
+      gameScore += 10000;
+      item.removeItem();
+      itemsArr.shift();
+      const displayScore = document.getElementById('showScore');
+      displayScore.innerText = gameScore;
+    }
+  });
+}, 10);
+
+// player keyboard control
 document.addEventListener('keydown', (e) => {
   if (e.code === 'ArrowRight') {
     clearInterval(player.intervalId);
